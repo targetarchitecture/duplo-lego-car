344a00e275cf15bde2e17c54d450c4635b19af05
Target Architecture <6796286+targetarchitecture@users.noreply.github.com>
2023-10-31 22:39:34 +0000

    Update blank credentials.h

--- src/include/blank credentials.h
+++ src/include/blank credentials.h
@@ -1,2 +1,16 @@
 
-MQTT_LOG_TOPIC
+// MQTT_LOG_TOPIC
+
+
+// #pragma once
+
+// auto WIFI_SSID = "";
+// auto WIFI_PASSWORD = "";
+
+// auto MQTT_SERVER = "";
+
+// auto MQTT_CLIENTID = "";
+// auto MQTT_USERNAME = "";
+// auto MQTT_KEY = "";
+
+// MQTT_LOG_TOPIC
